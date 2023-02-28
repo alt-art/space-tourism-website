e59de15b6276659b6bd05ad565a496072acc2718
Pedro Mendes <pedromendescraft@gmail.com>
2023-02-27 21:19:43 -0300

    feat(background): Background changes when navigating on routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
+import React from "react";
+import { Outlet, useLocation } from "react-router-dom";
 import Header from "./components/Header";
 
 function App() {
+  const location = useLocation();
+  console.log(location.pathname);
   return (
-    <div className="App">
+    <div
+      className={`App ${
+        location.pathname === "/"
+          ? "home"
+          : location.pathname.replaceAll("/", " ")
+      }`}
+    >
       <Header />
+      <Outlet />
     </div>
   );
 }
