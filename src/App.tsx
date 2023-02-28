5d40bf40d918d275ae96a1eb61130a1c1f791c7d
Pedro Mendes <pedromendescraft@gmail.com>
2023-02-28 15:42:41 -0300

    refactor(background): Change to styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,37 @@
 import React from "react";
 import { Outlet, useLocation } from "react-router-dom";
+import styled from 'styled-components';
 import Header from "./components/Header";
+
+interface AppStylesProps {
+  location: string;
+}
+
+const AppStyles = styled.div`
+  height: 100vh;
+  width: 100vw;
+  background-image: url("/background/${(props: AppStylesProps) => props.location}/desktop.jpg");
+  background-size: cover;
+  background-position: center;
+  @media (max-width: 768px) {
+    background-image: url("/background/${(props: AppStylesProps) => props.location}/tablet.jpg");
+  }
+  @media (max-width: 375px) {
+    background-image: url("/background/${(props: AppStylesProps) => props.location}/mobile.jpg");
+  }
+`;
 
 function App() {
   const location = useLocation();
   console.log(location.pathname);
   return (
-    <div
-      className={`App ${
-        location.pathname === "/"
-          ? "home"
-          : location.pathname.replaceAll("/", " ")
-      }`}
+    <AppStyles location={location.pathname === "/"
+    ? "home"
+    : location.pathname.split("/")[1]}
     >
       <Header />
       <Outlet />
-    </div>
+    </AppStyles>
   );
 }
 
